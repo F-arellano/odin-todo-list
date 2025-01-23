5576146f1f8ab9173fde903a6e012c1863375d68
F-arellano <pipe.arellano98@gmail.com>
2025-01-23 19:20:49 -0300

    ADD: todoList and Todo classes & minimum implementation to show class list

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 import "../css/reset.css";
 import "../css/styles.css";
+
+import { todoList } from "./todo";
+import { loadTodos } from "./load";
+
+loadTodos(todoList);
---
+++ src/js/todo.js
@@ -0,0 +1,37 @@
+class TodoList {
+  constructor() {
+    this.allTodos = [];
+  }
+
+  addTodo(todo) {
+    this.allTodos.push(todo);
+  }
+
+  removeTodo(todo) {
+    const index = this.allTodos.findIndex((item) => todo.id == item.id);
+    this.allTodos.splice(index, 1);
+  }
+}
+
+class ToDo {
+  id = 0;
+  constructor(text, date, priority, project = "home") {
+    this.text = text;
+    this.date = date;
+    this.priority = priority;
+    this.checked = false;
+    this.project = project;
+    this.id = ToDo.id;
+    ToDo.id++;
+  }
+
+  toggleState() {
+    this.checked = !this.checked;
+  }
+}
+
+let todoList = new TodoList();
+todoList.addTodo(new ToDo("Clean House 2", "14-23-11", 0));
+todoList.addTodo(new ToDo("Do laundry 2", "14-17-11", 1));
+
+export { todoList };
